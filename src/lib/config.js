8e665f0669f3cf94d66b51ed18349afefee22165
Michal <michal.danieluk@gmail.com>
2024-02-25 22:42:31 +0100

    Dodanie nowych komponentów

---
+++ src/lib/config.js
@@ -0,0 +1,6 @@
+import { dev } from '$app/environment';
+
+export const title = 'Amigos Maszyny';
+export const logo = 'Amigos LOGO';
+export const slogan = 'Naszym celem jest Twój sukces';
+export const url = dev ? 'http://localhost:5137/' : 'https://amigosmaszyny.pl/';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	//	import Footer from './lib/components/footer.svelte';
+
+	//import Header from './lib/components/header.svelte';
+	// import Footer from '$lib/components/footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
+	import Footer from '$lib/components/Footer.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<Footer />
